0443e9e9819b0667b46bc5287bff095a2b1fed98
jordanovx <jordanov2295@gmail.com>
2020-10-18 22:15:04 +0200

    Setup App and Screens

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
 
 /* Login Screen */
 
-export default class LoginScreen extends React.Component{
-    render (){
+export default function LoginScreen({navigation}){
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onFooterLinkPress = () => {
+        navigation.navigate('Registration')
+    }
+
+    const onLoginPress = () => {
+
+    }
+
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Login Screen test1</Text>
+            <View style={styles.container}>
+            <KeyboardAwareScrollView style={{ flex: 1, width: '100%'}} keyboardShouldPersistTaps='always' >
+                <Image style={styles.logo} />
+                <TextInput
+                    style={styles.input}
+                    placeholder = 'E-mail'
+                    placeholderTextColor = '#aaaaaa'
+                    onChangeText={(text) => setEmail(text)}
+                    value = {email}
+                    underlineColorAndroid = "transparent"
+                    autoCapitalize = 'none'
+                    />
+            </KeyboardAwareScrollView>
+            <Text>Login Screen test21</Text>
           </View>
         );
-    }
 
 
 
